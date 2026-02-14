9cfb4b27ab9611670d8473e78247cbf273ffaa90
Tan Sia How <tansiahow1996@gmail.com>
2026-02-14 16:18:46 +0000

    feat: add 'Flipped' tag to history items and style adjustments

--- src/components/History.vue
+++ src/components/History.vue
@@ -56,6 +56,7 @@
         <div class="history-item__layout">
           <div class="history-preview">
             <div v-if="item.status === 'pending'" class="history-tag">Pending</div>
+            <div v-if="item.settings?.isFlipped" class="history-tag history-tag--flipped">Flipped</div>
             <canvas :ref="el => setCanvasRef(item.id, el)" class="history-canvas" />
             <div class="history-actions">
               <button @click.stop="exportItem(item, false)" class="history-icon-btn" type="button"
@@ -104,7 +105,8 @@
         <p v-if="geminiApiKeyStatus" class="form-note">{{ geminiApiKeyStatus }}</p>
         <div class="dialog-actions">
           <button type="button" class="dialog-cancel" @click="closeSettingsModal">Close</button>
-          <button type="button" class="btn-secondary btn-secondary--alt" @click="clearGeminiApiKeySetting">Clear</button>
+          <button type="button" class="btn-secondary btn-secondary--alt"
+            @click="clearGeminiApiKeySetting">Clear</button>
           <button type="button" class="btn-primary" @click="saveGeminiApiKeySetting">Save API Key</button>
         </div>
       </div>
@@ -118,10 +120,8 @@
 import { ref, onMounted, onUnmounted, nextTick, watch } from 'vue'
 import { useHistory } from '../composables/useHistory'
 import { renderMiniature, renderCanvas } from '../utils/canvas'
-import { generateBMP, downloadBMP } from '../utils/bmp'
-import { extractNamesFromImage, getGeminiApiKey, saveGeminiApiKey } from '../utils/gemini'
 import { generateBMP, downloadBMP, overwriteBMP } from '../utils/bmp'
-import { extractNamesFromImage } from '../utils/gemini'
+import { extractNamesFromImage, getGeminiApiKey, saveGeminiApiKey } from '../utils/gemini'
 import ConfirmDialog from './ConfirmDialog.vue'
 import type { HistoryItem } from '../types'
 
